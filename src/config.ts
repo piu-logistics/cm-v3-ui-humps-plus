525dfccb1bd469dba0c0799eed6b811c22ba9eff
PIU-Logistics <44122622+piu-logistics@users.noreply.github.com>
2024-01-09 15:01:47 +0000

    Update config.ts main

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
 export const network = (process.env.NEXT_PUBLIC_SOLANA_NETWORK ||
-  WalletAdapterNetwork.Devnet) as WalletAdapterNetwork;
+  WalletAdapterNetwork.Mainnet) as WalletAdapterNetwork;
 // const network = WalletAdapterNetwork.Devnet;
 export const rpcHost =
   process.env.NEXT_PUBLIC_RPC_HOST || clusterApiUrl(network);
